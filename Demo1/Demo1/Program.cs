4aa17e9b5342940234a140cfb0369b1533ca2558
bloody rose <64304299+Fang1605@users.noreply.github.com>
2021-05-12 08:42:18 +0700

    Add files via upload

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Demo1
+{
+    class Program
+    {
+        void check(int ngay)
+        {
+            DateTime today = DateTime.Now;
+            int ngays = Convert.ToInt32(today.Day);
+            if( ngay == ngays)
+            {
+                Console.WriteLine("Happy brithday");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program A = new Program();
+            Console.WriteLine("Nhap ten");
+            String name = Console.ReadLine();
+            Console.WriteLine("Nhap day sinh");
+            String ngay = Console.ReadLine();
+            int ngays = int.Parse(ngay);
+            Console.WriteLine("Nhap tuoi");
+            String tuoi = Console.ReadLine();
+            int age = int.Parse(tuoi);
+            Console.WriteLine("Ten "  + name + "; ngay sinh " + ngays+ "; tuoi "+age);
+            A.check(ngays);
+            Console.ReadKey();
+
+        }
+    }
+}
